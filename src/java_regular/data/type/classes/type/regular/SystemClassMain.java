63a7300a3edec3134e8555c978c0bdd84f17552c
shmeli10 <shmeli10@gmail.com>
2020-08-02 12:15:09 +0300

    Add system class example code

---
+++ src/java_regular/data/type/classes/type/regular/SystemClassMain.java
@@ -0,0 +1,8 @@
+package java_regular.data.type.classes.type.regular;
+
+public class SystemClassMain {
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println("\nJava version is: " +version);
+    }
+}
