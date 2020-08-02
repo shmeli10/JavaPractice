6dffee245cecb70219a7b986d66ec7dc9da00e23
shmeli10 <shmeli10@gmail.com>
2020-08-02 14:12:50 +0300

    Add underscore keyword example code

---
+++ src/java_regular/syntax/keyword/UnderscoreMain.java
@@ -0,0 +1,10 @@
+package java_regular.syntax.keyword;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class UnderscoreMain {
+    public static void main(String[] args) {
+        String __ = "aaa";
+        System.out.println("Str: " +__);
+    }
+}
